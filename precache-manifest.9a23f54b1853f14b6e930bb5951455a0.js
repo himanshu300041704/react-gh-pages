29051cc35af54fa3b139150b2b45a81cd8e7ebe9
Himanshu Negi <himanshu.negi@myntra.com>
2020-05-02 00:52:59 +0530

    Updates

---
+++ precache-manifest.9a23f54b1853f14b6e930bb5951455a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2992edc9744e5ff9210c1b2967b90038",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "2883a2a755894a201450",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/react-gh-pages/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2883a2a755894a201450",
+    "url": "/react-gh-pages/static/js/main.21d2d30e.chunk.js"
+  },
+  {
+    "revision": "e7cbf49cba7e4a0e320f",
+    "url": "/react-gh-pages/static/js/runtime-main.efd8f609.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
